ad4f4c0fcd087eab995d2ccbeff23e58d624a762
ashrafulislam65 <md.ashraful.islam7758a@gmail.com>
2024-09-07 15:12:54 +0600

    added task1 fahrenheit conversion

---
+++ task1.js
@@ -0,0 +1,11 @@
+// Task-1:
+// Write a function to convert temperature from Celsius to Fahrenheit.
+function fahrenheit(celsius)
+{
+    let convert = (celsius * (9/5)) + 32;
+    return convert; 
+
+}
+let result = fahrenheit(104);
+console.log(result);
+
